ad4ba16ad5320965e1d16d791b2fd083db55b0af
CBothwell <bothwell.christopher@gmail.com>
2015-07-18 16:46:17 -0500

    working with xen

---
+++ bin/linode-depends.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+sudo bash -c 'echo "deb http://apt.linode.com/ $(lsb_release -cs) main" > /etc/apt/sources.list.d/linode.list'
+wget -O- https://apt.linode.com/linode.gpg | sudo apt-key add - 
+sudo apt-get install linode-cli
+
