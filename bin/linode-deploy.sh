e604b527fcbf75a89027f0c5805d0b7179856d4d
Christopher Bothwell <bothwell.christopher@gmail.com>
2015-07-22 22:10:54 -0500

    added bash and expect scripts

--- bin/linode-deploy.sh
+++ bin/linode-deploy.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
-# Create the menu.lst file
+echo "Shutting down linode instance...."
+ssh -t $LINODE_USER@$LISH_HOST $LINODE_NAME shutdown
+while [[ $(ssh -t $LINODE_USER@$LISH_HOST $LINODE_NAME status) =~ Running ]]
+  do sleep 3
+done
+echo "Starting Finnix..."
+ssh -t $LINODE_USER@$LISH_HOST $LINODE_NAME boot 2
+while [[ $(ssh -t $LINODE_USER@$LISH_HOST $LINODE_NAME status) =~ Powered ]]
+  do sleep 3
+done
+
+echo "Generating SSH key...."
+ssh-keygen -q -N ""
+
+echo "Sending SSH Key and starting SSH."
+expect bin/linode-finnix.expect
+
+echo "Generating the menu.lst file"
+# Create the menu.lst file#########################
 cat > menu.lst << EOF
 timeout 1
 
@@ -9,3 +27,14 @@ root (hd0)
 kernel /boot/mir-seal.xen root=/dev/xvda ro quiet
 EOF
 ##################################################
+
+echo "Mounting drives...."
+ssh root@$LINODE_IP "mount /dev/xvda /mirage"
+ssh root@$LINODE_IP "mkdir -p /mirage/boot/grub"
+
+echo "Moving kernel to Linode...."
+rsync -avP $PWD/mir-seal.xen root@$LINODE_IP:/mirage/boot/
+rsync -avP $PWD/menu.lst root@$LINODE_IP:/mirage/boot/grub/
+
+echo "Content Moved. Rebooting!"
+ssh -t $LINODE_USER@$LISH_HOST $LINODE_NAME reboot 1
