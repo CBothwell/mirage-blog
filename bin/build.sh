c809e65ac4339fa67006b08c12786e5d621d9525
Christopher Bothwell <bothwell.christopher@gmail.com>
2015-07-29 15:47:55 -0500

    fixed typo

--- bin/build.sh
+++ bin/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-if [[ ! -e $HOME/.opam ]]
+if [ ! -e $HOME/.opam ]
 then opam init -a; opam switch "4.01.0"
 fi
 eval `opam config env`
-if [[ ! -e $HOME/.opam/4.01.0/lib/mirage ]]
+if [ ! -e $HOME/.opam/4.01.0/lib/mirage ]
 then opam install -y mirage
 fi
-if [[ ! -e $HOME/.opam/4.01.0/lib/mirage-seal ]]
+if [ ! -e $HOME/.opam/4.01.0/lib/mirage-seal ]
 then opam install -y mirage-seal
 fi
 cd blog-src && jekyll build
