1cf1c9b78a8d0d26e409ecd527a261a593da031b
Christopher Bothwell <bothwell.christopher@gmail.com>
2015-07-29 13:03:05 -0500

    bash eof

--- bin/build.sh
+++ bin/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 if [[ ! -e $HOME/.opam ]]
-  then opam init -a --switch="4.01.0"
+then opam init -a --switch="4.01.0"
 if
 eval `opam config env`
 if [[ ! -e $HOME/.opam/4.01.0/lib/mirage ]]
-  then opam install -y mirage
+then opam install -y mirage
 fi
 if [[ ! -e $HOME/.opam/4.01.0/lib/mirage-seal ]]
-  then opam install -y mirage-seal
+then opam install -y mirage-seal
 fi
 cd blog-src && jekyll build
 cd ../ && mirage-seal --data=htdocs --no-tls --target=xen
