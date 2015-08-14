b728acb06e2eacccb69743eccaf2bbf758007d4a
Christopher Bothwell <bothwell.christopher@gmail.com>
2015-08-14 10:21:51 -0500

    updated build to use pin

--- bin/build.sh
+++ bin/build.sh
@@ -7,7 +7,7 @@ if [ ! -e $HOME/.opam/4.01.0/lib/mirage ]
 then opam install -y mirage
 fi
 if [ ! -e $HOME/.opam/4.01.0/lib/mirage-seal ]
-then opam pin add mirage-seal https://github.com/CBothwell/mirage-seal.git; opam install -y mirage-seal
+then opam pin -y add mirage-seal https://github.com/CBothwell/mirage-seal.git; opam install -y mirage-seal
 fi
 cd blog-src && jekyll build
 cd ../ && mirage-seal --data=htdocs --no-tls --target=xen
