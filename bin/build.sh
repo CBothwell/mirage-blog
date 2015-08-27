6942e9869f6a12a07a5b377e482b84dae7931635
Christopher Bothwell <bothwell.christopher@gmail.com>
2015-08-27 16:08:59 -0500

    updated bin/build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-if [ ! -e $HOME/.opam ]
-then opam init -a; opam switch "4.01.0"
-fi
+opam init -a #; opam switch "4.01.0"
 eval `opam config env`
-if [ ! -e $HOME/.opam/4.01.0/lib/mirage ]
-then opam install -y mirage
-fi
-if [ ! -e $HOME/.opam/4.01.0/lib/mirage-seal ]
-then opam pin -y add mirage-seal https://github.com/CBothwell/mirage-seal.git; opam install -y mirage-seal
-fi
+
+opam install -y mirage
+opam pin -y add mirage-seal https://github.com/CBothwell/mirage-seal.git; opam install -y mirage-seal
+
 cd blog-src && jekyll build
 cd ../ && mirage-seal --data=htdocs --no-tls --target=xen
